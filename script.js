10cbde8eadda6a866eb861c9092fa592cd9ce8d1
Ariel Ayash <ariel_a77@hotmail.com>
2026-04-30 21:15:19 +0300

    Add full screen mode

--- script.js
+++ script.js
@@ -155,8 +155,83 @@ function openEditIdeaForm(ideaId) {
     document.getElementById("edit-idea-form").addEventListener("submit", onSave);
 }
 
-document.getElementById("new-idea-form").addEventListener(
-    "submit",
+document.getElementById("free-text").addEventListener( "focusin",
+    () => {
+        document.querySelector(".edit-tool-bar").style.animation = "slideInTop 1s ease";
+        document.querySelector(".edit-tool-bar").classList.remove("hidden");
+    }
+);
+
+document.getElementById("free-text").addEventListener( "focusout",
+    () => {
+        if ( ! document.querySelector(".free-text-container").matches(':hover') ) {
+            document.querySelector(".edit-tool-bar").style.animation = "slideOutTop 1s ease";
+
+            setTimeout(
+                () => {
+                    document.querySelector(".edit-tool-bar").classList.add("hidden");
+                },
+                900
+            );            
+        }
+    }
+);
+
+// ------------------------------------------->
+//
+//    Callbacks for edit tool bar buttons
+//
+// ------------------------------------------->
+
+document.getElementById("fullscreen-btn").addEventListener( "click",
+    (e) => {                
+        e.preventDefault();  
+        document.getElementById("free-text").focus();      
+        if ( ! document.fullscreenElement ) {
+
+            if(document.querySelector(".free-text-container").requestFullscreen) {
+                
+                document.querySelector(".free-text-container").requestFullscreen();
+            }
+
+            
+            document.querySelector("textarea").rows = 50;            
+
+            document.getElementById("fullscreen-btn").innerHTML = `<i class="fa-solid fa-compress"></i>`;
+
+        }
+        else {
+            
+            if(document.exitFullscreen) {                
+                document.exitFullscreen();
+            }
+
+            
+            document.querySelector("textarea").rows = 10; // TODO: find a way to restore the value from html             
+
+            document.getElementById("fullscreen-btn").innerHTML = `<i class="fa-solid fa-expand"></i>`;
+        }
+
+        
+    }
+);
+
+// If the user exit the full screen without the button
+document.addEventListener('fullscreenchange', 
+    () => {
+        if (!document.fullscreenElement) {
+            document.querySelector("textarea").rows = 10; // TODO: find a way to restore the value from html             
+            document.getElementById("fullscreen-btn").innerHTML = `<i class="fa-solid fa-expand"></i>`;
+        }
+
+        document.getElementById("free-text").focus();
+    }
+);
+
+
+// --------------------------------------------------------------->
+
+document.getElementById("new-idea-form").addEventListener( "submit",
     (e) => {
 
         e.preventDefault();
@@ -174,24 +249,21 @@ document.getElementById("new-idea-form").addEventListener(
     }
 );
 
-document.getElementById("abort-idea-btn").addEventListener(
-    "click",
+document.getElementById("abort-idea-btn").addEventListener( "click",
     (e) => {
         e.preventDefault();
         openIdeasTab();
     }
 );
 
-document.getElementById("abort-edit-btn").addEventListener(
-    "click",
+document.getElementById("abort-edit-btn").addEventListener( "click",
     (e) => {
         e.preventDefault();
         openIdeasTab();
     }
 );
 
-document.getElementById("menu-btn").addEventListener(
-    "click",
+document.getElementById("menu-btn").addEventListener( "click",
     (e) => {
         e.preventDefault();
 
@@ -205,7 +277,6 @@ document.getElementById("menu-btn").addEventListener(
         }        
     }
 );
-
 
 async function saveStorageWithPicker() {
         
@@ -230,9 +301,7 @@ async function saveStorageWithPicker() {
     }
 }
 
-
-document.getElementById("export-option").addEventListener(
-    "click",
+document.getElementById("export-option").addEventListener( "click",
     (e) => {
         e.preventDefault();
 
@@ -267,8 +336,7 @@ async function loadStorageWithPicker() {
     }
 }
 
-document.getElementById("import-option").addEventListener(
-    "click",
+document.getElementById("import-option").addEventListener( "click",
     (e) => {
         e.preventDefault();
 
@@ -277,7 +345,6 @@ document.getElementById("import-option").addEventListener(
         loadStorageWithPicker();       
     }
 );
-
 
 window.onload = () => {
 
